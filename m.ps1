f6969f383b307706bae1b65b114e04407c34845f
leekspin <40032826+leekspin@users.noreply.github.com>
2018-06-12 22:42:40 +0200

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -1,6 +1,1 @@
-$url3 = "https://api.myjson.com/bins/thft2";
-$ip = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip;
-$contentType3 = "application/json; charset=utf-8";
 
-$json = "{`"a`" : `"$ip`"}";
-Invoke-RestMethod -Method PUT -Uri $url3 -ContentType $contentType3 -Body $json;
